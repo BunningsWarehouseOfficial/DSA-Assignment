ec2673dc703059c6630a7ee3314101b2b4926b9c
BunningsWarehouseOfficial <kristianrados40@gmail.com>
2019-10-28 01:46:37 +0800

    Added basic DS test harnesses

---
+++ src/QueueTestHarness.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class QueueTestHarness
+{
+    public static void main(String[] args)
+    {
+        DSAQueue queue = new DSAQueue();
+        test(queue);
+    }
+
+    private static void test(DSAQueue queue)
+    {
+        Scanner sc = new Scanner(System.in);
+        String cmd;
+        cmd = "";
+        do
+        {
+            try
+            {
+                System.out.println("Enter a string to add to the queue. " +
+                                   "Enter [-] to dequeue a term or enter " +
+                                   "[end] to quit.");
+                cmd = sc.nextLine();
+                if (cmd.equals("-"))
+                {
+                    System.out.println("Dequeued Term: " + queue.dequeue());
+                }
+                else
+                {
+                    queue.enqueue(cmd);
+                }
+            }
+            catch (IllegalArgumentException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        } while (!cmd.equals("end")); 
+    }
+}
